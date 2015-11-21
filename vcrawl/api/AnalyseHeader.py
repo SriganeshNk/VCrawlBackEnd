ea1b2e23dc8ed261f6000b42c5fd51f18a884f4e
balajisrinivas <balajisrinivas12@gmail.com>
2015-11-21 02:05:28 -0500

    Added code for checking CSP and HSTS

--- vcrawl/api/AnalyseHeader.py
+++ vcrawl/api/AnalyseHeader.py
@@ -6,24 +6,47 @@
 
 class AnalyseHeader(object):
     def __init__(self):
-        self.CSP = ["content-security-policy", "x-content-security-policy", "x-webkit-csp"]
+        self.CSP = "content-security-policy"
         self.Xframe = ["x-frame-options", "frame-options"]
         self.XSS = "x-xss-protection"
         self.CSRF = "nonce"
-        self.HTTPS = "strict-transport-security"
+        self.HSTS = "strict-transport-security"
 
     def checkCSP(self, header):
-        for each in self.CSP:
-            if each in header:
-                #Probably need to have additional checks, like what kind of CSP is secure
-                return True
-        return False
+	csp_directives = ['base-uri', 'child-src', 'connect-src', 'default-src', 'font-src', 'form-action', 'frame-ancestors', 'frame-src', 'img-src', 'media-src', 'object-src', 'plugin-types', 'report-uri','script-src', 'style-src', 'upgrade-insecure-requests']
+	csp_map = {'implemented' : 'NO'}
+	
+	if self.CSP in header:
+		csp_map['implemented'] = 'YES'
+		for directive in csp_directives:
+			csp_map[directive] = 'NO'
+		policy_string = header[self.CSP]
+		policy_list = policy_string.split(';')
+		for policy in policy_list:
+			policy_strings = policy.strip().split(' ')
+			if policy_strings[0] in csp_directives:
+				csp_map[policy_strings[0]] = 'YES'
+	return csp_map
 
-    def checkHTTPS(self, header):
-        if self.HTTPS in header:
-            #Probably need to have additional checks, like what kind of HTTPS is enabled
-            return True
-        return False
+    def checkHSTS(self, header):
+        hsts_directives = ['max-age', 'includeSubDomains', 'preload']
+	hsts_map = {'implemented' : 'NO'}
+
+	if self.HSTS in header:
+		hsts_map['implemented'] = 'YES'
+		for directive in hsts_directives:
+			hsts_map[directive] = 'NO'
+		policy_string = header[self.HSTS]
+		policy_list = policy_string.split(';')
+		for policy in policy_list:
+			policy_strings = policy.strip().split('=')
+			print policy, policy_strings
+			if policy_strings[0] in hsts_directives:
+				if policy_strings[0] == hsts_directives[0]:
+					hsts_map[policy_strings[0]] = policy_strings[1]
+				else:
+					hsts_map[policy_strings[0]] = 'YES' 
+        return hsts_map
 
     def checkXFrame(self, header):
         for each in self.Xframe:
@@ -48,11 +71,11 @@ class AnalyseHeader(object):
         vul = {}
         vul['csp'] = self.checkCSP(header)
         vul['csrf'] = self.checkCSRF(header)
-        vul['https'] = self.checkHTTPS(header)
+        vul['hsts'] = self.checkHSTS(header)
         vul['xframe'] = self.checkXFrame(header)
         vul['xss'] = self.checkXSS(header)
         return vul
-"""
+
 a = AnalyseHeader()
 import httplib2
 import json
@@ -60,4 +83,4 @@ h = httplib2.Http(".cache")
 (header, content) = h.request("http://twitter.com/jimmyfallon", "GET")
 print json.dumps(header, indent=4, sort_keys=True)
 print json.dumps(a.checkURLS(header, content), indent=4, sort_keys=True)
-"""
+
