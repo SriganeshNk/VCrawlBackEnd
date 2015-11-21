888f67df0a053f3e51bb2bdf1d33e656e7437ff8
balajisrinivas <balajisrinivas12@gmail.com>
2015-11-21 17:16:33 -0500

    Map contains boolean values, added code to check XSS/XFrame options

--- vcrawl/api/AnalyseHeader.py
+++ vcrawl/api/AnalyseHeader.py
@@ -7,49 +7,49 @@
 class AnalyseHeader(object):
     def __init__(self):
         self.CSP = "content-security-policy"
-        self.Xframe = ["x-frame-options", "frame-options"]
+        self.XFRAME = ["x-frame-options", "frame-options"]
         self.XSS = "x-xss-protection"
         self.CSRF = "nonce"
         self.HSTS = "strict-transport-security"
 
     def checkCSP(self, header):
 	csp_directives = ['base-uri', 'child-src', 'connect-src', 'default-src', 'font-src', 'form-action', 'frame-ancestors', 'frame-src', 'img-src', 'media-src', 'object-src', 'plugin-types', 'report-uri','script-src', 'style-src', 'upgrade-insecure-requests']
-	csp_map = {'implemented' : 'NO'}
+	csp_map = {'implemented' : False}
 	
 	if self.CSP in header:
-		csp_map['implemented'] = 'YES'
+		csp_map['implemented'] = True
 		for directive in csp_directives:
-			csp_map[directive] = 'NO'
+			csp_map[directive] = False
 		policy_string = header[self.CSP]
 		policy_list = policy_string.split(';')
 		for policy in policy_list:
 			policy_strings = policy.strip().split(' ')
 			if policy_strings[0] in csp_directives:
-				csp_map[policy_strings[0]] = 'YES'
+				csp_map[policy_strings[0]] = True
 	return csp_map
 
     def checkHSTS(self, header):
         hsts_directives = ['max-age', 'includeSubDomains', 'preload']
-	hsts_map = {'implemented' : 'NO'}
+	hsts_map = {'implemented' : False}
 
 	if self.HSTS in header:
-		hsts_map['implemented'] = 'YES'
+		hsts_map['implemented'] = True
 		for directive in hsts_directives:
-			hsts_map[directive] = 'NO'
+			hsts_map[directive] = False
 		policy_string = header[self.HSTS]
 		policy_list = policy_string.split(';')
 		for policy in policy_list:
 			policy_strings = policy.strip().split('=')
-			print policy, policy_strings
 			if policy_strings[0] in hsts_directives:
 				if policy_strings[0] == hsts_directives[0]:
 					hsts_map[policy_strings[0]] = policy_strings[1]
 				else:
-					hsts_map[policy_strings[0]] = 'YES' 
+					hsts_map[policy_strings[0]] = True 
         return hsts_map
 
     def checkXFrame(self, header):
-        for each in self.Xframe:
+        '''
+	for each in self.Xframe:
             if each in header:
                 tmp = header[each].lower()
                 if tmp == 'deny' or tmp == 'sameorigin' or tmp.startswith('allow-from'):
@@ -58,9 +58,27 @@ class AnalyseHeader(object):
                     return False
             else:
                 return False 
-               
+        '''
+	xframe_modes = ['deny', 'sameorigin', 'allow-from']
+	xframe_map = {'implemented' : False}
+	
+	for xframe in self.XFRAME:
+		if xframe in header:
+			xframe_map['implemented'] = True 
+	      		policy_string = header[xframe]
+			policy_list = policy_string.split(';')
+			assert len(policy_list) == 1, "Can specify only one XFrame mode!"
+			policy_strings = policy_list[0].split(' ')
+			if policy_strings[0] in xframe_modes:
+				xframe_map['mode'] = policy_strings[0]
+			if policy_strings[0] == 'allow-from':
+				assert len(policy_strings) == 2, "Need an external framing page URI for 'allow-from' framing mode"
+				xframe_map['framing_page_uri'] = policy_strings[1]
+	return xframe_map	
+
     def checkXSS(self, header):
-        if self.XSS in header:
+        '''
+	if self.XSS in header:
             #if "" in header[self.XSS]:
             #    return False    
             if "mode=block" in header[self.XSS]:
@@ -69,7 +87,26 @@ class AnalyseHeader(object):
                 return True 
             else:
 		return False            
-
+	'''
+	xss_directives = ['mode']
+	xss_map = {'implemented' : False}
+	
+	if self.XSS in header:
+		for directive in xss_directives:
+			xss_map[directive] = False
+		policy_string = header[self.XSS]
+		policy_list = policy_string.split(';')
+		for policy in policy_list:
+			policy_strings = policy.strip().split('=')
+			if policy_strings[0] == '1':
+				xss_map['implemented'] = True
+			elif policy_strings[0] == xss_directives[0]:
+				if policy_strings[1] == 'block':
+					xss_map['mode'] = 'block'
+				else:
+					xss_map['mode'] = 'sanitize'
+	return xss_map
+	
     def checkCSRF(self, header):
         if self.CSRF in header:
             #Probably need to have additional checks, like what kind of CSRF is enabled
