5990b9b3bdc3a9cb3995456fc4d43907a0aa7d7e
sriganeshnk <sriganesh.navaneethakrishnan@stonybrook.edu>
2015-11-22 03:45:52 -0500

    CSP bug fix

--- vcrawl/api/AnalyseHeader.py
+++ vcrawl/api/AnalyseHeader.py
@@ -3,6 +3,8 @@
 # Based on the above website information about http headers
 
 # Nonces ---> need to check forms for nonce id's, requires parsing the content and identifying nonceIDS
+from bs4 import BeautifulSoup
+
 
 class AnalyseHeader(object):
     def __init__(self):
@@ -13,11 +15,11 @@ class AnalyseHeader(object):
         self.HSTS = "strict-transport-security"
 
     def checkCSP(self, header):
+        #print "CSP"
         csp_directives = ['base-uri', 'child-src', 'connect-src', 'default-src', 'font-src', 'form-action', 'frame-ancestors', 'frame-src', 'img-src', 'media-src', 'object-src', 'plugin-types', 'report-uri','script-src', 'style-src', 'upgrade-insecure-requests']
         csp_map = {'implemented' : False}
-
         for csp in self.CSP:
-            if self.CSP in header:
+            if csp in header:
                 csp_map['implemented'] = True
                 for directive in csp_directives:
                     csp_map[directive] = False
@@ -27,12 +29,12 @@ class AnalyseHeader(object):
                     policy_strings = policy.strip().split(' ')
                     if policy_strings[0] in csp_directives:
                         csp_map[policy_strings[0]] = True
-            return csp_map
+        return csp_map
 
     def checkHSTS(self, header):
+        #print "hsts"
         hsts_directives = ['max-age', 'includeSubdomains', 'preload']
         hsts_map = {'implemented' : False}
-
         if self.HSTS in header:
             hsts_map['implemented'] = True
             for directive in hsts_directives:
@@ -50,9 +52,9 @@ class AnalyseHeader(object):
         return hsts_map
 
     def checkXFrame(self, header):
+        #print "xframe"
         xframe_modes = ['deny', 'sameorigin', 'allow-from']
         xframe_map = {'implemented' : False}
-
         for xframe in self.XFRAME:
             if xframe in header:
                 xframe_map['implemented'] = True
@@ -68,9 +70,9 @@ class AnalyseHeader(object):
         return xframe_map
 
     def checkXSS(self, header):
+        #print "xss"
         xss_directives = ['mode']
         xss_map = {'implemented' : False}
-
         if self.XSS in header:
             for directive in xss_directives:
                 xss_map[directive] = False
@@ -88,11 +90,11 @@ class AnalyseHeader(object):
         return xss_map
 
     def checkCSRF(self, header, content):
+        #print "csrf"
         nonce_fields = ['appActionToken', 'secTok', 'authenticity_token', 'nonce']
         for each in self.CSP:
-            if each in header:
-                if 'nonce' in header[each]:
-                    return True
+            if each in header and 'nonce' in header[each]:
+                return True
         soup = BeautifulSoup(content, "lxml")
         for form in soup.find_all('form'):
             for formAttrs in form.attrs:
@@ -113,6 +115,8 @@ class AnalyseHeader(object):
         vul['xframe'] = self.checkXFrame(header)
         vul['xss'] = self.checkXSS(header)
         return vul
+
+
 """
 a = AnalyseHeader()
 import httplib2
